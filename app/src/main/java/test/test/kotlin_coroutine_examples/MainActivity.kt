20afeb8edfef60d1311df820be6a7f8198b40f81
Sana Ebadi <sanaebadi97@gmail.com>
2019-11-29 14:28:28 +0330

    init commit

---
+++ app/src/main/java/test/test/kotlin_coroutine_examples/MainActivity.kt
@@ -0,0 +1,12 @@
+package test.test.kotlin_coroutine_examples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
